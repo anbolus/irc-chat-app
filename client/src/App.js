6af902c3514ab63069f624e6eb2ba4e0c9c95508
Andrea Bolus <andrea.bolus@epitech.eu>
2023-02-06 14:10:54 +0100

    setup socket.io server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './views/Register'
 import Login from './views/Login'
 import Chat from './views/Chat'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:5000');
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js";
-import corsOptions from "./config/cors.js";
+import http from "http";
 import cors from 'cors';
+import { Server } from "socket.io";
 const port = 5000;
 app.use(express.json());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 
 app.use("/api/auth/", authRoutes);
@@ -16,18 +17,47 @@ app.use("/api/auth/", authRoutes);
     res.send("ok");
 }) */
 
+const server = http.createServer(app);
 
-app.get("/api/auth/login/", (req, res) =>{
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected to ${socket.id}`)
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with id : ${socket.id} joined room ${data}`);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+
+app.get("/api/auth/login/", (req, res) => {
     res.send('login');
 })
 
-app.get("/api/auth/register/", (req, res) =>{
+app.get("/api/auth/register/", (req, res) => {
     res.send('register');
 })
 
-const server = app.listen(5000, () => {
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+})
+
+
 
 
 mongoose.connect(uri, {
